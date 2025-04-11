b84398282df96ca4555ba8a34872c4a7d8054e05
nguyenvukhanguit <khangnguyen231@outlook.com.vn>
2025-04-11 21:16:54 +0700

    check out

--- urls.py
+++ urls.py
@@ -1,24 +1,29 @@
 from django.urls import path
-from core.views import index, product_list_view, category_list_view, \
-    category_product_list_view, product_detail_view, tag_list, ajax_add_review,\
-    search_view, add_to_cart, cart_view, checkout_view, delete_item_from_cart, response
-app_name= 'core'
+from core.views import (
+    index, product_list_view, category_list_view,
+    category_product_list_view, product_detail_view, tag_list,
+    ajax_add_review, search_view, add_to_cart, cart_view,
+    checkout_view, delete_item_from_cart, response
+)
 
-urlpatterns=[
-    path("",index, name='index'),
-    path("products/",product_list_view, name="product-list"),
+app_name = 'core'
+
+urlpatterns = [
+    path("", index, name='index'),
+    path("products/", product_list_view, name="product-list"),
     path("category/", category_list_view, name="category-list"),
-    path("category/<cid>/",category_product_list_view,name="category-product-list"), 
-    path("product/<pid>/",product_detail_view , name="product-detail" ),
+    path("category/<cid>/", category_product_list_view, name="category-product-list"), 
+    path("product/<pid>/", product_detail_view, name="product-detail"),
     path("products/tag/<slug:tag_slug>/", tag_list, name="tags"),
-    path("ajax-add-review/<pid>/",ajax_add_review, name="ajax-add-review"),
-    path("search/",search_view,name="search"),
-    path("add-to-cart/",add_to_cart, name="add-to-cart"),
+    path("ajax-add-review/<pid>/", ajax_add_review, name="ajax-add-review"),
+    path("search/", search_view, name="search"),
+
+    # Cart and Checkout
     path("cart/", cart_view, name="cart"),
+    path("add-to-cart/", add_to_cart, name="add-to-cart"),
     path("delete-from-cart/", delete_item_from_cart, name="delete-from-cart"),
     path("checkout/", checkout_view, name="checkout"),
-    path('response', response, name='response'),
 
-    #checkout URL
-    path("checkout/", checkout_view, name="checkout")
+    # Response
+    path('response/', response, name='response'),
 ]
